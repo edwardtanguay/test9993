46c48c1a6b4b0e015b3a72308c08e50482e262a2
Edward Tanguay (windows) <edward@tanguay.info>
2023-07-21 11:29:12 +0200

    init commit

---
+++ src/main.ts
@@ -0,0 +1,38 @@
+import './style.css'
+import employees from './data/employees.json';
+
+// interface IUser {
+//   firstName: string;
+//   lastName: string;
+//   age: number;
+//   greet: () => string;
+// }
+
+// const user1: IUser = {
+//   firstName: 'Kyle',
+//   lastName: 'Thomsonson',
+//   age: 7,
+//   greet: function () {
+//     return '';
+//   }
+// }
+
+// user1.greet = function () {
+//   return 'he is ' + this.age;
+// }
+
+let html = '';
+for (const employee of employees) {
+  html += `
+  <div class="employee">
+  <div>${employee.address.phone} ${employee.lastName}</div> 
+  </div>
+  `;
+}
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <h1>There are ${employees.length} employees.</h1>
+    ${html}
+  </div>
+`
